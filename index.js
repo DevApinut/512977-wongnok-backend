19c8152dfc3732adfc219342e054b2bf9dfabfde
DevApinut <Apinut.dev@gmail.com>
2024-05-21 13:30:38 +0700

    t

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 
 
 const express = require("express")
-// const morgan = require("morgan")
-// const cors = require("cors")
-// const mongoose = require("mongoose")
+const morgan = require("morgan")
+const cors = require("cors")
+const mongoose = require("mongoose")
 
 // require("dotenv").config()
 
-// const RouteAPI = require('./Component/Route/RouteAPI')
+const RouteAPI = require('./Component/Route/RouteAPI')
 
 const app = express()
 
 
 //For middleware
 app.use(express.json())
-// app.use(cors())
-// app.use(morgan("dev"))
+app.use(cors())
+app.use(morgan("dev"))
 
 //For set /api/....
 app.get('/', (req, res) => {
     res.send('ทดสอบระบบ Backend')
 })
-// app.use('/api', RouteAPI)
+app.use('/api', RouteAPI)
 
 const port = 9000
 app.listen(port, () => { console.log(`Success for connect to Server at port ${port}`) })
 
-// //connect to mongo dB
-// mongoose.connect(process.env.DATABASE, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: false
-// })
-//     .then(() => console.log("เชื่อมต่อข้อมูลสำเร็จ"))
-//     .catch((err) => console.log("การเชื่อมต่อมีปัญหา"))
+//connect to mongo dB
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: false
+})
+    .then(() => console.log("เชื่อมต่อข้อมูลสำเร็จ"))
+    .catch((err) => console.log("การเชื่อมต่อมีปัญหา"))
 
 
 
