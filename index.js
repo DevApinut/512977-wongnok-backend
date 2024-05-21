c757bf8f9f81e3b222a28dc832b1239c908014dd
DevApinut <Apinut.dev@gmail.com>
2024-05-21 13:36:14 +0700

    vercel

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express()
 
 //For middleware
 app.use(express.json())
-// app.use(cors())
-// app.use(morgan("dev"))
+app.use(cors())
+app.use(morgan("dev"))
 
 //For set /api/....
 app.get('/', (req, res) => {
@@ -27,12 +27,12 @@ const port = 9000
 app.listen(port, () => { console.log(`Success for connect to Server at port ${port}`) })
 
 //connect to mongo dB
-// mongoose.connect(process.env.DATABASE, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: false
-// })
-//     .then(() => console.log("เชื่อมต่อข้อมูลสำเร็จ"))
-//     .catch((err) => console.log("การเชื่อมต่อมีปัญหา"))
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: false
+})
+    .then(() => console.log("เชื่อมต่อข้อมูลสำเร็จ"))
+    .catch((err) => console.log("การเชื่อมต่อมีปัญหา"))
 
 
 
