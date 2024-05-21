4b22bde151eb3c1eafb0944478d62b87fb352b4f
DevApinut <Apinut.dev@gmail.com>
2024-05-21 13:37:42 +0700

    vercel

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
 const express = require("express")
-// const morgan = require("morgan")
-// const cors = require("cors")
-// const mongoose = require("mongoose")
+const morgan = require("morgan")
+const cors = require("cors")
+const mongoose = require("mongoose")
 
 // require("dotenv").config()
 
