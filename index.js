8ca96cbc94ace61367465f6b78edb46a4c35dcd3
DevApinut <Apinut.dev@gmail.com>
2024-06-01 20:17:59 +0700

    commit

--- index.js
+++ index.js
@@ -16,28 +16,29 @@ const RouteAPI = require('./Component/Route/RouteAPI')
 const app = express()
 
 
- 
+
 //For middleware
 // app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
-
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// 
+// Save file mord than 50 mb
+// 
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
-
-
-
-//For set /api/....
-app.get('/test', (req, res) => {
-    res.send(`${process.env.DATABASE}`)
-})
+// 
+//For set /api/....{API Router}
+// 
 app.use('/api', RouteAPI)
-
+//
+// Open port at 9000
+// 
 const port = 9000
 app.listen(port, () => { console.log(`Success for connect to Server at port ${port}`) })
-
-//connect to mongo dB
+// 
+// connect to mongo dB
+// 
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: false
